ea7fd185cd46a45aa034a1cfd60ed5f9b842ee0b
Margot Gunning <margot.gunning2017@gmail.com>
2017-11-28 11:37:36 -0800

    testing

--- tests/testthat/test_box_cox.R
+++ tests/testthat/test_box_cox.R
@@ -1,13 +1,32 @@
-context("Box-cox transformations on non-numerics")
+context("Box-cox transformations on non-numerics and string outputs")
 
+#expect that all four will pass
 test_that("Numerics work, and that the inverse gives the raw response (separate tests).", {
 	expect_identical(box_cox_transform(1,0), 0)
-	expect_identical(box_cox_transform(1,0), 0)
+	expect_identical(box_cox_transform(3,2), 4)
 	expect_identical(inverse_box_cox_transform(0,0), 1)
+	expect_identical(inverse_box_cox_transform(4,2),3)
 })
 
+#expect that both will pass
 test_that("Logicals convert to numerics.", {
 	logic_response = TRUE
 	logic_param = FALSE
 	expect_identical(box_cox_transform(logic_response, logic_param), box_cox_transform(1,0))
+	expect_identical(inverse_box_cox_transform(logic_response, logic_param), inverse_box_cox_transform(1,0))
+})
+
+#expect that this will pass
+test_that("The string output is correct (custom 'error' message).",{
+	string <- "Cannot log transform response < 0, check inputs"
+	expect_that(box_cox_transform(-1,0), matches(string))
+	expect_that(box_cox_transform(-1,0), is_a("chr"))
+
+})
+
+#expect that this will fail
+test_that("The string output is correct (custom 'error' message).",{
+	string <-"Number not real, check input"
+	expect_that(inverse_box_cox_transform(1,-1), matches(string))
+	expect_that(inverse_box_cox_transform(1,-1), is_a("chr"))
 })
--- tests/testthat/test_square.R
+++ tests/testthat/test_square.R
@@ -1,18 +1,40 @@
 context("Squaring and cubing non-numerics")
 
+#expect to pass; this is from the class notes
 test_that("At least numeric values work.", {
 	num_vec <- c(0, -4.6, 3.4)
 	expect_identical(square(numeric(0)), numeric(0))
 	expect_identical(square(num_vec), num_vec^2)
 })
 
+
+#expect to pass; this is from the class ntoes
 test_that("Logicals automatically convert to numeric.", {
 	logic_vec <- c(TRUE, TRUE, FALSE)
 	expect_identical(square(logic_vec), logic_vec^2)
 })
 
+#expect to pass; this is the cube version of above
 test_that("The cube fuction also works for numeric values.", {
 	num_vec2 <- c(1,3,6)
 	expect_identical(cube(numeric(0)), numeric(0))
 	expect_identical(cube(num_vec2), num_vec2^3)
+})
+
+#expect to pass; this is the cube version of above
+test_that("Logicals automatically convert to numeric.", {
+	logic_vec <- c(TRUE, TRUE, FALSE)
+	expect_identical(cube(logic_vec), logic_vec^3)
+})
+
+#expect to pass
+test_that("What happens with a string?",{
+	string <- "Squared vector"
+	expect_that(square(string),
+				throws_error("non-numeric argument"))
+})
+
+#expect to fail
+test_that("That output is not a string",{
+	expect_that(cube(3), is_a("chr"))
 })
